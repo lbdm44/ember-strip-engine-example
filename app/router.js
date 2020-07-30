b467cbea35f6cebf0a49a2cf5f138ba1b218fcd6
Lewis Miller <(none)>
2020-07-30 13:03:02 -0700

    first attempt at production stripping

--- app/router.js
+++ app/router.js
@@ -1,14 +1,20 @@
-import EmberRouter from '@ember/routing/router';
-import { DEBUG } from '@glimmer/env';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import { DEBUG } from "@glimmer/env";
+import config from "./config/environment";
 
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function() {
+Router.map(function () {
+  this.route('index');
+
   if (DEBUG) {
-    this.mount('foo-bar');
+    this.mount("foo-bar", {
+      as: "foo-bar",
+      path: "/foo-bar",
+      resetNamespace: true,
+    });
   }
 });
